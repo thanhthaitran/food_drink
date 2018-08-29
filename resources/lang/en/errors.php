cbb2b0803dec0a394ef8edd22e8404afea4a94f1
Thai Tran T <thai.tran@asiantech.vn>
2018-08-29 10:43:13 +0700

    api create address

--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -3,4 +3,6 @@
         'title' => 'Not find user',
         'message' => 'Can not find user with corresponding id.',
         'update_fail' => 'Update Fail',
+        'delete_fail' => 'Deleted Fail',
+        'create_fail' => 'Created Fail',
     ];
