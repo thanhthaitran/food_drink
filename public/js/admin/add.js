8451f5150b138da950fde2cb804051de4a7b97d8
thanhthaitran <thaitran.sts@gmail.com>
2018-09-27 15:27:20 +0700

    edit note

--- public/js/admin/add.js
+++ public/js/admin/add.js
@@ -70,4 +70,49 @@ $(document).ready(function () {
     $('#note-change-order .modal-body #note').val('');
     $('#note-change-order').modal('hide');
   });
-})
+
+  // edit note
+  $(document).on('click', '.edit-note', function(event) {
+    event.preventDefault();
+    var id = $(this).attr('edit-id');
+    action = $(this).attr('data-action');
+    if(action == 'view') {
+      $('#note' + id).replaceWith(function() {
+        return '<input class="form-control" id="input'+ id +'" type="text" value ="' + $('#note'+id).text() +'" />';
+      });
+      $(this).attr('data-action', 'edit');
+    } else {
+      content = $('#input'+ id).val();
+      $('.modal-body .alert-info').html('');
+      $('.modal-body .alert-danger').html('');
+      $.ajax({
+        url: '/admin/note/'+ id,
+        type: 'PUT',
+        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+        dataType: 'json', 
+        data: {
+          "content": content,
+        },
+        success: function (response){
+          $('#input'+ id).replaceWith(function () {
+              return '<p id="note'+ id +'">'+ response.content +'<p/>';
+          });
+          $('.modal-body .alert-info').html(Lang.get('order.admin.show.edit_success'));
+          $('.modal-body .alert-info').show();
+        },
+        error: function(response) {
+          errorMessage = Lang.get('order.admin.show.edit_not_success') +'<br/>'+ response.responseJSON.message +'<br/>';
+          if (response.responseJSON.errors) {
+            errors = Object.keys(response.responseJSON.errors);
+            errors.forEach(error => {
+              errorMessage += response.responseJSON.errors[error] + '<br/>';
+            });
+          }
+          $('.modal-body .alert-danger').html(errorMessage);
+          $('.modal-body .alert-danger').show();
+        }
+      });
+      $(this).attr('data-action', 'view');
+    }
+  });
+});
